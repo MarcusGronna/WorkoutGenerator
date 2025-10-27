149608b5b6dbaee3342d951cf20fae16002066d0
Marcus Grönnå <mgronna@gmail.com>
2025-10-27 11:01:37 +0100

    Add Project for demo

---
+++ WorkoutGenerator.Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using WorkoutGenerator.Core;
+using System;
+using System.IO;
+using System.Text.Json;
+
+string filePath = PathHelper.DataFile("exercises.json");
+Console.WriteLine($"Full path to data file:\n{filePath}");
+
+
+string content = File.ReadAllText(filePath);
+using JsonDocument doc = JsonDocument.Parse(content);
+string prettyJson = JsonSerializer.Serialize(
+    doc.RootElement,
+    new JsonSerializerOptions { WriteIndented = true }
+);
+
+Console.WriteLine("=== FORMATTED JSON ===\n");
+Console.WriteLine(prettyJson);
