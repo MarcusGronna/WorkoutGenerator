028fe60994d47314ecfdde06bba324468f5a901b
Marcus Grönnå <mgronna@gmail.com>
2025-10-27 12:00:29 +0100

    Add: implementation of GenerateWorkout in Factory

--- WorkoutGenerator.Core/WorkoutFactory.cs
+++ WorkoutGenerator.Core/WorkoutFactory.cs
@@ -2,5 +2,23 @@
 
 public class WorkoutFactory
 {
-    // public void
+    public Workout GenerateWorkout(List<Exercise> exercises, string muscleGroup, GoalType goalType)
+    {
+        var workout = Workout.Create(exercises, muscleGroup, goalType);
+
+        switch (goalType)
+        {
+            case GoalType.Strength:
+                workout.Repetitions = 6;
+                break;
+            case GoalType.Hypertrophy:
+                workout.Repetitions = 15;
+                break;
+            default:
+                workout.Repetitions = 10;
+                break;
+        }
+
+        return workout;
+    }
 }
