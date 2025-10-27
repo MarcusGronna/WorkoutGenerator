de0f71328234e01f9dd44763df79107f1995b04b
Marcus Grönnå <mgronna@gmail.com>
2025-10-27 11:46:08 +0100

    Refactor: constructor private. New method to create instance

--- WorkoutGenerator.Core/Models/Workout.cs
+++ WorkoutGenerator.Core/Models/Workout.cs
@@ -1,6 +1,22 @@
 namespace WorkoutGenerator.Core;
 
-public class FileReader
+public class Workout
 {
+    public List<Exercise> Exercises { get; set; }
+    public string MuscleGroup { get; set; }
+    public GoalType GoalType { get; set; }
 
+    private Workout() { }
+
+    public static Workout Create(List<Exercise> exercises, string muscleGroup, GoalType goalType)
+    {
+        var w = new Workout();
+
+        w.Exercises = exercises;
+        w.MuscleGroup = muscleGroup;
+        w.GoalType = goalType;
+
+
+        return w;
+    }
 }
